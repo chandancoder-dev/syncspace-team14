1a3e4c7faffbca704f79ee0ff6ea2628682514d2
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-07-16 22:59:52 +0530

    Initialize dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>Dashboard page under development.</p>
+      <p>Dashboard page under development </p>
     </div>
   );
 }
