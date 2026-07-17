ef697f604cba606332b83f576a9eaeca4737d222
ShaikSabeha-123 <23ve5a0526@sreyas.ac.in>
2026-07-17 22:07:11 +0530

    feat: implement password visibility toggle

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,32 +1,52 @@
 import "../styles/login.css";
+import { useState } from "react";
 
 function Login() {
-  return (
-    <div className="login-container">
-      <div className="login-card">
-        <h1>Welcome Back</h1>
-        <p>Sign in to continue to SyncSpace</p>
+    const [showPassword, setShowPassword] = useState(false);
 
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        
-        <div className="show-password">
-        <input type="checkbox" id="showPassword" />
-        <label htmlFor="showPassword">Show Password</label>
+    return (
+        <div className="login-container">
+            <div className="login-card">
+
+                <h1>Welcome Back</h1>
+                <p>Sign in to continue to SyncSpace</p>
+
+                <input
+                    type="email"
+                    placeholder="Email"
+                />
+
+                <input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                />
+
+                <div className="show-password">
+                    <input
+                        type="checkbox"
+                        id="showPassword"
+                        checked={showPassword}
+                        onChange={() => setShowPassword(!showPassword)}
+                    />
+                    <label htmlFor="showPassword">
+                        Show Password
+                    </label>
+                </div>
+
+                <div className="forgot-password">
+                    <a href="#">Forgot Password?</a>
+                </div>
+
+                <button>Login</button>
+
+                <p className="register-text">
+                    Don't have an account?{" "}
+                    <a href="#">Register</a>
+                </p>
+
+            </div>
         </div>
-
-        <div className="forgot-password">
-          <a href="#">Forgot Password?</a>
-        </div>
-
-        <button>Login</button>
-
-        <p className="register-text">
-          Don't have an account? <a href="#">Register</a>
-        </p>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
