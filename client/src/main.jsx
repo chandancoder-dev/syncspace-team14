535e0fc7d158926717184873728c2f96a8080546
Sumit Verma <sumit.verma.aie@gmail.com>
2026-07-18 13:28:40 +0530

    Standardize Navbar component and stylesheet naming

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import "./styles/variables.css"
 import "./styles/global.css"
-import "./styles/navbar.css"
 import "./styles/button.css"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
