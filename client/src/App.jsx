d7e2b3de7a3f941e006216aa7e3970d760ca29d3
Sumit Verma <147837554+sumitverma21@users.noreply.github.com>
2026-07-20 21:58:18 +0530

    Revert "Deepan feature"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,34 @@
-import { Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./components/About";
+import Features from "./components/Features";
+import Login from "./components/Login";
+import NavBar from "./components/Navbar";
+import CreateRoom from "./pages/CreateRoom";
+import WorkSpace from "./pages/workspace/WorkSpace";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const location = useLocation();
+
+  const hideNav =
+    location.pathname.startsWith("/workspace") ||
+    location.pathname.startsWith("/dashboard");
+
   return (
-    <Routes>
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      {!hideNav && <NavBar />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-room" element={<CreateRoom />} />
+        <Route path="/workspace" element={<WorkSpace />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 }
 
