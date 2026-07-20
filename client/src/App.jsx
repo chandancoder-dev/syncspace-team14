ecae8f3a71a6dbe0bb61d568e3572cff292b6a85
deepan <deepanmurali63@gmail.com>
2026-07-20 20:53:06 +0530

    Message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,11 @@
-import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./components/About";
-import Features from "./components/Features";
-import Login from "./components/Login";
-import NavBar from "./components/Navbar";
-import CreateRoom from "./pages/CreateRoom";
-import WorkSpace from "./pages/workspace/WorkSpace";
-import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
-  const location = useLocation();
-
-  const hideNav =
-    location.pathname.startsWith("/workspace") ||
-    location.pathname.startsWith("/dashboard");
-
   return (
-    <>
-      {!hideNav && <NavBar />}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create-room" element={<CreateRoom />} />
-        <Route path="/workspace" element={<WorkSpace />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+    </Routes>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import App from "./App.jsx";
-
-import "./styles/variables.css";
-import "./styles/global.css";
-import "./styles/button.css";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
