f6d10d97c8ebe9ec2bcd4943e13f0f757cc2d651
pratiksha-dighe123 <dpratiksha292@gmail.com>
2026-07-21 17:48:00 +0530

    Update dashboard UI with Blue Serenity theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App.jsx";
 
-import "./styles/variables.css";
-import "./styles/global.css";
-import "./styles/button.css";
+import "./index.css";
+
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
