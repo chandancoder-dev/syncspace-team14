7aae78d1529ea2b53afe5de44bf71055ef8b2598
Nilabha <n.samadder.h4578@gmail.com>
2026-07-21 12:45:55 +0530

    deleting duplicate controllers , routes and models and shift them inside src directory.

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,9 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-import express from "express";
-import mongoose from "mongoose"
-import userRouter from "../routers/routes.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/syncspaceDB")
-.then(()=> console.log("db connected"))
-
-
-app.use("/api",userRouter);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
