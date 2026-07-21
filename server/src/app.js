b0ef835434259e0c7df7e68e69997fb8501f605f
Nilabha <n.samadder.h4578@gmail.com>
2026-07-21 20:25:12 +0530

    sending login credentails to backend.

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,22 @@ import express from "express";
 import cors from "cors";
 import roomRoutes from "./routes/room.routes.js";
 import userRoutes from "./routes/user.routes.js"
+import mongoose from "mongoose";
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+const corsOptions = {
+    origin: "http://localhost:5173", 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+};
+app.use(cors(corsOptions));
+//Database connecttion.
+mongoose.connect("mongodb://127.0.0.1:27017/syncspaceDB")
+.then(()=>console.log("database connected"))
+.catch((e)=>console.log(e));
 // Routes
 app.use("/api/rooms", roomRoutes);
 app.use("/api",userRoutes);
