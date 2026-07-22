986dae801cc0962c4ca16969362ee9d1a728fafb
ShaikSabeha-123 <23ve5a0526@sreyas.ac.in>
2026-07-22 22:07:48 +0530

    Complete authentication module and frontend login integration

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,15 @@
 import http from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
-import cors from "cors";
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import roomHandler from "./socket/roomHandler.js";
 
 dotenv.config();
 
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-import roomHandler from "./socket/roomHandler.js";
-
-dotenv.config();
-
-const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
@@ -27,13 +22,14 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("A user connected");
+
   roomHandler(io, socket);
 
   socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
-}
-);
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
