bdddb0cb7075f2f634cb583845a34ac8f6fbd5da
ShaikSabeha-123 <23ve5a0526@sreyas.ac.in>
2026-07-24 14:45:11 +0530

    Change backend port from 5000 to 8000

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -36,7 +36,7 @@ function Login() {
 
   try {
     const res = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "http://localhost:8000/api/auth/login",
       {
         email,
         password,
--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
